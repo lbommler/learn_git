f3459862bb613b150e8717f6913fbba63aad6579
Hans <you@example.com>
2021-01-23 11:07:40 +0100

    positive message fx added

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ x = 3
 def add_13(x):
     return x + 13
     
-
 message = "homework sucks"
+
+def positive_msg(msg):
+    print(msg.replace('sucks', 'is great!'))
+    return None
+
+positive_msg(message)
